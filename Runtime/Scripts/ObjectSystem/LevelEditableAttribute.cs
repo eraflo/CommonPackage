6ed936c4e3c57e21dd419258b0d4f47d41ed3845
Florian Maillard <81640753+eraflo@users.noreply.github.com>
2026-01-29 02:30:51 +0100

    fix: adde show in inspector bool to level editable attribute

--- Runtime/Scripts/ObjectSystem/LevelEditableAttribute.cs
+++ Runtime/Scripts/ObjectSystem/LevelEditableAttribute.cs
@@ -5,6 +5,11 @@ namespace Eraflo.Common.ObjectSystem
     [AttributeUsage(AttributeTargets.Field)]
     public class LevelEditableAttribute : Attribute
     {
+        public bool ShowInInspector { get; set; } = true;
 
+        public LevelEditableAttribute(bool showInInspector = true)
+        {
+            ShowInInspector = showInInspector;
+        }
     }
 }
